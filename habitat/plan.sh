15f07d2cc7254e43b824ceffb9551cdd1635f05f
Collin McNeese <cmcneese@chef.io>
2020-08-15 00:57:37 -0500

    v1.6.115
    Signed-off-by: Collin McNeese <cmcneese@chef.io>

--- habitat/plan.sh
+++ habitat/plan.sh
@@ -1,6 +1,6 @@
 pkg_name=hab_sup_service
 pkg_origin=collinmcneese
-pkg_version="1.6.56"
+pkg_version="1.6.115"
 pkg_maintainer="Collin McNeese <cmcneese@chef.io>"
 pkg_license=("Apache-2.0")
 pkg_deps=(core/hab-sup)
